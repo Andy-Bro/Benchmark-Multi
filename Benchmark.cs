1088e9969f0fd2984efce58e2dd9e478ea3fa039
AndyBro <andybro@gmx.de>
2022-12-11 11:25:19 +0100

    Add class "Benchmark". In there create functions for starting Multi-Threading Bench. In "MainWindow" function Button_Click start bench.

---
+++ Benchmark.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+using System.Diagnostics;
+
+namespace Benchmark_Multi
+{
+    public class Benchmark : MainWindow
+    {
+        public static int TurnsAllThreads = 1;
+
+        public int GetTurnsAllThreads()
+        {
+            return TurnsAllThreads;
+        }
+
+        public void SetTurnsAllThreads()
+        {
+            TurnsAllThreads += 1;
+        }
+
+        public static void ThreadPower()
+        {
+            Thread.Sleep(100);
+
+            int testValue;
+
+            while (TurnsAllThreads < 50)
+            {
+                testValue = 0;
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    testValue += i;
+                }
+
+                TurnsAllThreads += 1;
+            }
+        }
+
+        public void StartBench()
+        {
+            Thread tPower0 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower1 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower2 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower3 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower4 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower5 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower6 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower7 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower8 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower9 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower10 = new Thread(new ThreadStart(ThreadPower));
+            Thread tPower11 = new Thread(new ThreadStart(ThreadPower));
+
+            tPower0.Start();
+            tPower1.Start();
+            tPower2.Start();
+            tPower3.Start();
+            tPower4.Start();
+            tPower5.Start();
+            tPower6.Start();
+            tPower7.Start();
+            tPower8.Start();
+            tPower9.Start();
+            tPower10.Start();
+            tPower11.Start();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Diagnostics;
+using System.Threading;
+
 namespace Benchmark_Multi
 {
     /// <summary>
@@ -28,6 +31,20 @@ namespace Benchmark_Multi
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             lbl_message.Content = "Please Wait...";
+
+            Benchmark bench = new Benchmark();
+            bench.StartBench();
+
+            while (bench.GetTurnsAllThreads() < 50)
+            {
+                Thread.Sleep(1000);
+
+                bench.SetTurnsAllThreads();
+
+                lbl_result.Content = "Turns: " + bench.GetTurnsAllThreads().ToString();
+            }
+
+            lbl_message.Content = "Benchmark Finished";
         }
     }
 }
