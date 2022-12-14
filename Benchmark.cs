03a89606fc261b7663dc3b20519667981365a72a
AndyBro <andybro@gmx.de>
2022-12-14 03:49:30 +0100

    Add second benchmark

--- Benchmark.cs
+++ Benchmark.cs
@@ -12,26 +12,31 @@ namespace Benchmark_Multi
 {
     public class Benchmark
     {
-        public static int TurnsAllThreads = 1;
+        public static int TurnsAllThreads = 0;
 
         public static int MaxTurnsAllThreads { get; set; }
         public static int IncrementTop { get; set; }
+        public static int StringSearchNumber { get; set; }
         public static int LogicalProcessors { get; set; }
 
 
-        public Benchmark(int _maxTurnsAllThreads, int _incrementTop, int _logicalProcessors)
+        public Benchmark(int _maxTurnsAllThreads, int _incrementTop, int _stringSearchNumber, int _logicalProcessors)
         {
             MaxTurnsAllThreads = _maxTurnsAllThreads;
             IncrementTop = _incrementTop;
+            StringSearchNumber = _stringSearchNumber;
+
             LogicalProcessors = _logicalProcessors;
 
-            TurnsAllThreads = 1;
+            TurnsAllThreads = 0;
         }
 
         public int GetTurnsAllThreads()
         {
             return TurnsAllThreads;
         }
+
+
 
         public static void ThreadPowerIncrement()
         {
@@ -43,6 +48,28 @@ namespace Benchmark_Multi
                 for (int i = 0; i < upToIncrement; i++)
                 {
 
+                }
+
+                TurnsAllThreads += 1;
+            }
+        }
+
+        public static void ThreadPowerStringSearch()
+        {
+            Thread.Sleep(100);
+
+            int upToIncrement = StringSearchNumber;
+            string searchFor = "Hallo World " + StringSearchNumber;
+            bool findString = false;
+
+            while (TurnsAllThreads < MaxTurnsAllThreads)
+            {
+                for (int i = 0; i < upToIncrement; i++)
+                {
+                    if (searchFor == "Hallo World " +i)
+                    {
+                        findString = true;
+                    }
                 }
 
                 TurnsAllThreads += 1;
@@ -74,6 +101,120 @@ namespace Benchmark_Multi
             Thread tPower20 = new Thread(new ThreadStart(ThreadPowerIncrement));
             Thread tPower21 = new Thread(new ThreadStart(ThreadPowerIncrement));
             Thread tPower22 = new Thread(new ThreadStart(ThreadPowerIncrement));
+
+
+
+            if (LogicalProcessors == 1)
+            {
+                tPower0.Start();
+            }
+            else if (LogicalProcessors == 2)
+            {
+                tPower0.Start();
+                tPower1.Start();
+            }
+            else if (LogicalProcessors == 4)
+            {
+                tPower0.Start();
+                tPower1.Start();
+                tPower2.Start();
+            }
+            else if (LogicalProcessors == 8)
+            {
+                tPower0.Start();
+                tPower1.Start();
+                tPower2.Start();
+                tPower3.Start();
+                tPower4.Start();
+                tPower5.Start();
+                tPower6.Start();
+            }
+            else if (LogicalProcessors == 12)
+            {
+                tPower0.Start();
+                tPower1.Start();
+                tPower2.Start();
+                tPower3.Start();
+                tPower4.Start();
+                tPower5.Start();
+                tPower6.Start();
+                tPower7.Start();
+                tPower8.Start();
+                tPower9.Start();
+                tPower10.Start();
+                // tPower11.Start();
+            }
+            else if (LogicalProcessors >= 16 && LogicalProcessors < 24)
+            {
+                tPower0.Start();
+                tPower1.Start();
+                tPower2.Start();
+                tPower3.Start();
+                tPower4.Start();
+                tPower5.Start();
+                tPower6.Start();
+                tPower7.Start();
+                tPower8.Start();
+                tPower9.Start();
+                tPower10.Start();
+                tPower11.Start();
+                tPower12.Start();
+                tPower13.Start();
+                tPower14.Start();
+            }
+            else if (LogicalProcessors >= 24)
+            {
+                tPower0.Start();
+                tPower1.Start();
+                tPower2.Start();
+                tPower3.Start();
+                tPower4.Start();
+                tPower5.Start();
+                tPower6.Start();
+                tPower7.Start();
+                tPower8.Start();
+                tPower9.Start();
+                tPower10.Start();
+                tPower11.Start();
+                tPower12.Start();
+                tPower13.Start();
+                tPower14.Start();
+                tPower15.Start();
+                tPower16.Start();
+                tPower17.Start();
+                tPower18.Start();
+                tPower19.Start();
+                tPower20.Start();
+                tPower21.Start();
+                tPower22.Start();
+            }
+        }
+
+        public void StartBenchStringSearch()
+        {
+            Thread tPower0 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower1 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower2 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower3 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower4 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower5 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower6 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower7 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower8 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower9 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower10 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower11 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower12 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower13 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower14 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower15 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower16 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower17 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower18 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower19 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower20 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower21 = new Thread(new ThreadStart(ThreadPowerStringSearch));
+            Thread tPower22 = new Thread(new ThreadStart(ThreadPowerStringSearch));
 
 
 
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,11 +25,15 @@ namespace Benchmark_Multi
     public partial class MainWindow : Window
     {
         // ------------ Options ------------
-        private int maxTurns = 140;
-        private int IncrementTop = 500000000;
+        private int maxTurns = 800;
+        private int incrementTop = 100000000;
+        private int searchValueString = 1000000;
 
         private int logicalProcessors = Environment.ProcessorCount;
 
+        private double swBenchIncrement = 0;
+        private double swBenchStringSearch = 0;
+        
 
         public MainWindow()
         {
@@ -49,20 +53,36 @@ namespace Benchmark_Multi
                         DoubleAnimation doubleanimation = new DoubleAnimation(100.0, duration);
                         progbar.BeginAnimation(ProgressBar.ValueProperty, doubleanimation);*/
 
-            Bench();
+            lbl_message.Content = "Benchmark 1 of 2. Please Wait";
+
+            await SimulateProgressBar(pBar0);
+
+            BenchIncrement();
+
+            await Task.Delay(3000);
+
+            await SimulateProgressBar(pBar0);
+
+            BenchStringSearch();
+        }
+
+        
+        public static async Task SimulateProgressBar(ProgressBar pBar)
+        {
+            pBar.Value = 0;
 
             for (int i = 0; i < 101; i++)
             {
-                await Task.Delay(100);
+                await Task.Delay(40);
 
-                pBar0.Value += i;
+                pBar.Value += i;
             }
-
         }
 
-        private void Bench()
+
+        private void BenchIncrement()
         {
-            Benchmark bench = new Benchmark(maxTurns, IncrementTop, logicalProcessors);
+            Benchmark bench = new Benchmark(maxTurns, incrementTop, searchValueString, logicalProcessors);
             bench.StartBenchIncrement();
 
             Thread.Sleep(100);
@@ -75,9 +95,33 @@ namespace Benchmark_Multi
 
             SwIncrement.Stop();
 
-            lbl_message.Content = "Benchmark Finished";
-            lbl_result.Content = "Time: " + SwIncrement.Elapsed.TotalMilliseconds + "ms";
+            lbl_message.Content = "Benchmark 2 of 2. Please Wait";
+
+            swBenchIncrement = Math.Round(SwIncrement.Elapsed.TotalMilliseconds, 2);
+            lbl_result.Content = "Bench 1: " + swBenchIncrement + "ms";
             // lbl_result.Content = "Time: " + SwIncrement.Elapsed.TotalMilliseconds / (double)maxTurns + "ms";
+        }
+
+        private void BenchStringSearch()
+        {
+            Benchmark bench = new Benchmark(maxTurns, incrementTop, searchValueString, logicalProcessors);
+            bench.StartBenchStringSearch();
+
+            Thread.Sleep(100);
+
+            var Sw = Stopwatch.StartNew();
+            while (bench.GetTurnsAllThreads() < maxTurns)
+            {
+                Thread.Sleep(1);
+            }
+
+            Sw.Stop();
+
+            lbl_message.Content = "Benchmark Complete";
+
+            swBenchStringSearch = Math.Round(Sw.Elapsed.TotalMilliseconds, 2);
+            lbl_result.Content = "Benchmark Total: " + (swBenchStringSearch + swBenchIncrement) + "ms" + " Bench 1: " + swBenchIncrement + "ms" + " Bench 2: " + swBenchStringSearch + "ms";
+
         }
     }
 }
